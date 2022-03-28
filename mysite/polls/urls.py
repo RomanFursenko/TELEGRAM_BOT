567c58a2aad2b9435063b39af10765dd7fe8ca4e
Roman Fursenko <furroma@gmail.com>
2022-03-28 13:26:08 +0200

    Initial commit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='Опрос'),
     path('<int:question_id>/', views.detail, name='detail'),
