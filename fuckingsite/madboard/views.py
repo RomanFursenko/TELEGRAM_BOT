90be90ef8f36cc08e1e545a559ccb1392dc8a65d
Roman Fursenko <furroma@gmail.com>
2022-04-11 16:11:46 +0300

    Repair myfirstapp

--- fuckingsite/madboard/views.py
+++ fuckingsite/madboard/views.py
@@ -6,5 +6,5 @@ from .models import Mb
 def index(request):
     template = loader.get_template('madboard/index.html')
     mbs = Mb.objects.order_by('-published')
-    context = {'mbs':mbs}
+    context = {'mbs': mbs}
     return HttpResponse(template.render(context, request))
--- fuckingsite/myfirstapp/views.py
+++ fuckingsite/myfirstapp/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+
 from .models import Fa
 
 def index(request):
     template = loader.get_template('myfirstapp/index.html')
+    fas = Fa.objects.all()
     context = {'fas':fas}
     return HttpResponse(template.render(context, request))
